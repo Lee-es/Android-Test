985f17ad2aec2f4a110bfdb44d6524cf3bc9ff4f
yuisk789 <yuisk789@gmail.com>
2020-11-27 09:29:02 +0900

    android start

--- app/src/main/java/org/techtown/test/MainActivity.java
+++ app/src/main/java/org/techtown/test/MainActivity.java
@@ -1,14 +1,20 @@
 package org.techtown.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
